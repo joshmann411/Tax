db13e9c9cf58f53bc354fecff681fa9d59f9035c
joshmann <joshmann411@gmail.com>
2019-08-19 18:16:10 +0200

    Tax Unit Test included

---
+++ TaxCalculatorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TaxCalculatorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaxCalculatorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5d611b94-0773-4561-8406-377e31d4115c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaxCalculatorUnitTest/Tax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaxWebApp.Controllers;
+using TaxService.Models;
+using TaxService.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TaxCalculatorUnitTest
+{
+    [TestClass]
+    public class Tax
+    {
+        [TestMethod]
+        public void DBConnectionTest()
+        {
+            TaxCalculatorDBEntities dBEntities = new TaxCalculatorDBEntities();
+
+            CalculatedTax tx = dBEntities.CalculatedTaxes.Find(5);
+
+            Assert.IsTrue(tx != null);
+        }
+    }
+}
